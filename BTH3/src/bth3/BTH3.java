a78f41954a003a0f5c0ac6d5693894a82c3cb66a
= <=>
2018-04-18 20:44:39 +0700

    1804

--- BTH3/src/bth3/BTH3.java
+++ BTH3/src/bth3/BTH3.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package th2;
+package bth3;
 
 /**
  *
- * @author ngochoak57
+ * @author Admin
  */
-public class TH2 {
+public class BTH3 {
 
     /**
      * @param args the command line arguments
--- JTable_VD/src/jtable_vd/JTable_VD.java
+++ JTable_VD/src/jtable_vd/JTable_VD.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package th2;
+package jtable_vd;
 
 /**
  *
- * @author ngochoak57
+ * @author Admin
  */
-public class TH2 {
+public class JTable_VD {
 
     /**
      * @param args the command line arguments
